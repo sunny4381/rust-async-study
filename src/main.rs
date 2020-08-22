651436d1291c76810c8dcedcb841a18eec1f1114
NAKANO Hideo <pinarello.marvel@gmail.com>
2020-08-22 14:41:05 +0900

    manually implements async function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use std::future::Future;
 use futures::executor::block_on;
 
 async fn hello_world() {
     println!("hello, world!");
 }
 
+fn hello_world2() -> impl Future<Output = ()> {
+    async {
+        println!("hello, world2!");
+    }
+}
+
 async fn async_main() {
-    hello_world().await;
+    futures::join!(hello_world(), hello_world2());
 }
 
 fn main() {
